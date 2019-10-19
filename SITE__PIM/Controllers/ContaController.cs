84a7831917b00e1590de822e6f1bc243c94a6e95
Samuel Julio <you@example.com>
2019-10-19 17:20:57 -0300

    Tela login

---
+++ SITE__PIM/Controllers/ContaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SITE__PIM.Models;
+using System.Data.SqlClient; // habilita conexão com sql
+
+namespace SITE__PIM.Controllers
+{
+    public class ContaController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Conta
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionSting()
+        {
+            con.ConnectionString = "data source=PC; database= ; integrated security = SSPI;"; // colocar informações do banco de dados
+        }
+        [HttpPost]
+        public ActionResult verificar(Conta acc)
+        {
+            connectionSting();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from where username='"+acc.Nome+"'and password='"+acc.Senha+"'"; // parte referente a entrada de nome e senha na pagina de login
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+
+                return View("Sucesso");
+            }
+            else
+            {
+                con.Close();
+
+                return View("Erro");
+            }
+            
+         
+        }
+    }
+}
---
+++ SITE__PIM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SITE__PIM.Global" Language="C#" %>
